87479f7b460cb4b9a91f6c184d05bf78751b234a
Rydwane <113770575+Rydwane@users.noreply.github.com>
2022-10-13 20:49:42 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+char str1[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1,str1,59);
+return (1);
 }
