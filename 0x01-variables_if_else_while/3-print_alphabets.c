2319daf7f6827d04e86e591ee3f9efc9e2fc49b6
Rydwane <113770575+Rydwane@users.noreply.github.com>
2022-10-14 15:00:06 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <time.h>
 
@@ -31,37 +30,3 @@ putchar('\n');
 return (0);
 
 }
-=======
-#include <stdio.h>
-#include <time.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-
-{
-int n = 97;
-
-int c = 65;
-
-while (n <= 122)
-{
-putchar(n);
-n++;
-}
-while (c <= 90)
-{
-putchar(c);
-c++;
-}
-
-putchar('\n');
-
-return (0);
-
-}
->>>>>>> 8b81156d699501145045a72a174005a491ea23ca
