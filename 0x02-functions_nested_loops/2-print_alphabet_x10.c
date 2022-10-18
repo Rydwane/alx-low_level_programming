fdae24f6d3f845313d9ba2ed092051b0de457d01
Rydwane <113770575+Rydwane@users.noreply.github.com>
2022-10-18 18:07:43 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,26 +9,11 @@
 void print_alphabet(void)
 {
 
-	char i;
+char i;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-}
-
-/**
- * print_alphabet_x10 - prints aphabet 10 times
- *
- **/
-void print_alphabet_x10(void)
+for (i = 'a'; i <= 'z'; i++)
 {
-
-	int i;
-
-	for (i = '0'; i <= '9'; i++)
-	{
-		print_alphabet();
-	}
+_putchar(i);
+}
+_putchar('\n');
 }
