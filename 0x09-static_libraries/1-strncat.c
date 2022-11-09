f8c5e1695005de39100bb7a760a35bf44cc9c880
Rydwane <113770575+Rydwane@users.noreply.github.com>
2022-11-09 22:34:43 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,1 +1,17 @@
-char *_strncat(char *dest, char *src, int n);
+#include "main.h"
+#include<string.h>
+
+/**
+ * _strncat- function that concatenates two strings.
+ *
+ * @dest: pointer to destination char
+ * @src: pointer to source char
+ * @n: number of bytes to receive
+ * Return: char
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+strncat(dest, src, n);
+return (dest);
+}
