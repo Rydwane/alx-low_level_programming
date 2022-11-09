acd53921aa05489300612d74ad6d3cbc0aba7bbe
Rydwane <113770575+Rydwane@users.noreply.github.com>
2022-11-09 22:35:38 +0100

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,1 +1,16 @@
-char *_strncpy(char *dest, char *src, int n);
+
+#include "main.h"
+#include<string.h>
+/**
+ * _strncpy -  function that copies a string..
+ * @src: cadena a copiar
+ * @dest: destino de la cadena.
+ * @n: number.
+ * Return: void
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n);
+return (dest);
+}
