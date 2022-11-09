dc4c11200e55d858b36e24a81e5b83efa794cee7
Rydwane <113770575+Rydwane@users.noreply.github.com>
2022-11-09 23:53:22 +0100

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,21 @@
-int _abs(int n);
+
+#include "main.h"
+
+
+/**
+ * _abs - computes absolute value
+ * @n: int to compute as absolute
+ * Return: absolute value
+ **/
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
