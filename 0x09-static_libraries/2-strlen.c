f34da590955be8a0b19b74c78624a209897f2a29
Rydwane <113770575+Rydwane@users.noreply.github.com>
2022-11-09 23:24:07 +0100

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,11 @@
-int _strlen(char *s);
+#include "main.h"
+#include "string.h"
+/**
+ * _strlen - finds the length of the string
+ * @s: stirng
+ * Return: void
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
