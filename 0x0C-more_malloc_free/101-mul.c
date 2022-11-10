2ec0cfd9a2da587d2550b88b1c276c7e2bef0414
Rydwane <113770575+Rydwane@users.noreply.github.com>
2022-11-11 00:37:16 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -119,9 +119,9 @@ _putchar(e[ti]);
 exit(98);
 }
 for (l1 = 0; argv[1][l1]; l1++)
-		;
+;
 for (l2 = 0; argv[2][l2]; l2++)
-		;
+;
 ln = l1 + l2 + 1;
 a = malloc(ln * sizeof(char));
 if (a == NULL)
