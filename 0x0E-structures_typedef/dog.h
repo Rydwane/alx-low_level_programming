f96e265ce30861266719adf1bc84428d1438a29f
Rydwane <113770575+Rydwane@users.noreply.github.com>
2022-11-15 18:30:11 +0100

    Update dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - a structure of dog
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the ownerof the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+int _putchar(char);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+#endif /* MAIN_H */
