d0e5204d14d97e6e893107e77086f09aa29703f9
Rydwane <113770575+Rydwane@users.noreply.github.com>
2022-11-17 20:25:21 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -44,6 +44,9 @@ int op_div(int a, int b)
 {
 if (b != 0)
 return (a / b);
+
+printf("Error\n");
+exit(100);
 }
 
 /**
@@ -56,4 +59,7 @@ int op_mod(int a, int b)
 {
 if (b != 0)
 return (a % b);
+
+printf("Error\n");
+exit(100);
 }
