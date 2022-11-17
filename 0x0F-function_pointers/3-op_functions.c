23e52236fcf52315777f370d1e16890e2721aabd
Rydwane <113770575+Rydwane@users.noreply.github.com>
2022-11-17 19:55:52 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,63 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include "3-calc.h"
 /**
- * op_add - addition
- * @a: number 1
- * @b: number 2
- * Return: the addend of the two numbers
+ * get_op_func - choose the operatin to performded
+ * @s: a string
+ * Return: a pointer to a function
  */
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-return (a + b);
-}
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
 
-/**
- * op_sub - Substraction
- * @a: number 1
- * @b: number 2
- * Return: the substraction of the two numbers
- */
-int op_sub(int a, int b)
+for (i = 0; ops[i].op != NULL; i++)
 {
-return (a - b);
+if (s[0] == ops->op[i])
+break;
 }
-
-/**
- * op_mul - multiplication
- * @a: number 1
- * @b: number 2
- * Return: the multiplication of the two numbers
- */
-int op_mul(int a, int b)
-{
-return (a * b);
-}
-
-/**
- * op_div - division
- * @a: number 1
- * @b: number 2
- * Return: the questiont of the two numbers
- */
-int op_div(int a, int b)
-{
-if (b != 0)
-return (a / b);
-printf("Error\n");
-exit(100);
-}
-
-/**
- * op_mod - modules
- * @a: number 1
- * @b: number 2
- * Return: the modules of the two numbers
- */
-int op_mod(int a, int b)
-{
-if (b != 0)
-return (a % b);
-printf("Error\n");
-exit(100);
+return (ops[i / 2].f);
 }
